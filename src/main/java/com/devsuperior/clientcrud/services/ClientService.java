51ead1ca080aff5f4f06fbcb0268aed66fec53de
OruamC <oru_catani@hotmail.com>
2020-10-05 20:28:37 -0300

    GET client by Id

--- src/main/java/com/devsuperior/clientcrud/services/ClientService.java
+++ src/main/java/com/devsuperior/clientcrud/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.clientcrud.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.clientcrud.dto.ClientDTO;
 import com.devsuperior.clientcrud.entities.Client;
 import com.devsuperior.clientcrud.repositories.ClientRepository;
+import com.devsuperior.clientcrud.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -20,6 +23,13 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 	
 }
